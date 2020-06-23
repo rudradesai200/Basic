c2313b93a3444953ce8fc3b570b99890704cafd2
rudra <rudradesai200@gmail.com>
2020-06-23 08:57:28 +0530

    Adding colors to Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 echo -n "Enter you Python3 Path ($(which python3)) : " && read pythonpath
 if [ -z "$pythonpath" ]
 then
@@ -15,17 +18,17 @@ version=$($BASICPYTHONPATH -V 2>&1 | grep -Po '(?<=Python )(.+)')
 parsedVersion=$(echo "${version//./}")
 if [ "$parsedVersion" -gt "270" ]
 then 
-    echo "Python version > 2.7.0 OK"
+    echo -e "Python version > 2.7.0 .... ${GREEN}OK${NC}"
 else
-    echo "Please install Python3 before proceeding"
+    echo -e "${RED}Please install Python3 before proceeding${NC}"
     exit 1
 fi
 
 echo "Checking for django-admin installation"
 if which django-admin &> /dev/null; then
-    echo "django-admin installation found OK"
+    echo -e "django-admin installation found .... ${GREEN}OK${NC}"
 else
-    echo 'django-admin not found'
+    echo -e "${RED}django-admin not found${NC}"
     echo 'Installing Django'
     if [ which pip3 &> /dev/null ] ; then
         check=$(which pip)
@@ -42,10 +45,10 @@ else
     echo "$BASICPIPPATH install Django --no-cache-dir"
     $BASICPIPPATH install Django --no-cache-dir
     if [ $? -eq 0 ]; then
-        echo "Successfully Installed Django"
+        echo -e "${GREEN}Successfully Installed Django${NC}"
     else
-        echo "Please manually install Django"
-        echo "And run this script again"
+        echo -e "${RED}Please manually install Django${NC}"
+        echo -e "${RED}And run this script again${NC}"
         exit 4
     fi
 fi
@@ -55,10 +58,10 @@ djangover=$(django-admin version)
 requiredver=2.2
 if [ $djangoover >= $requiredver ]
 then
-    echo "Django version >= 2.2 OK"
+    echo -e "Django version >= 2.2 ..... ${GREEN}OK${NC}"
 else
     echo "Django version $djangover"
-    echo "Please install Django version >= 2.2 for this script to work"
+    echo -e "${RED}Please install Django version >= 2.2 for this script to work${NC}"
     exit 2
 fi
 
