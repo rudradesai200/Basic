b80439d36419e1eb853524519d8838b4855929e3
rudradesai200 <rudradesai200@gmail.com>
2020-12-12 17:27:26 +0530

    --> Added authentication app --> Minor bugs removal --> Comments improved

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -3,7 +3,7 @@
 DJANGOADMIN=$(which django-admin)
 GREEN='\033[0;32m'
 RED='\033[0;101m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -12,7 +12,7 @@ then
     PROJECTNAME=$2
 	$DJANGOADMIN startproject $PROJECTNAME
     if [ $? -eq 0 ]; then
-        echo -e "A new Django Project created with name $PROJECTNAME ... ${GREEN}${bold}OK${normal}${NC}"
+        echo -e "Create new project named $PROJECTNAME ... ${GREEN}${bold}OK${normal}${NC}"
     else
         echo -e "${RED}${bold}Django Admin installation issues found${normal}${NC}"
         echo -e "${RED}${bold}Please check your installation${normal}${NC}"
@@ -28,7 +28,7 @@ then
     mkdir $PROJECTNAME/secret/
     touch $PROJECTNAME/secret/__init__.py
     cp $BASIC_DIR/adders/addsettings.py $PROJECTNAME/secret/
-    echo -e "Created a folder named secret to keep sensitive data ... ${GREEN}${bold}OK${normal}${NC}"
+    echo -e "Create a folder named secret to keep sensitive data ... ${GREEN}${bold}OK${normal}${NC}"
 
     # Set up other directories
 	mkdir $PROJECTNAME/static
@@ -37,16 +37,16 @@ then
 
     # Sets up settings.py for secret folder
     $BASIC_DIR/scripts/settings_setup.sh $PROJECTNAME
-    echo -e "Setup secret folder used and transfered variables ... ${GREEN}${bold}OK${normal}${NC}"
+    echo -e "Setup secret folder and setup.py ... ${GREEN}${bold}OK${normal}${NC}"
 
     # Adding gitignore to the folder
     cp $BASIC_DIR/adders/.gitignore $PROJECTNAME/
-    echo -e "Adding gitignore to the folder path ... ${GREEN}${bold}OK${normal}${NC}"
+    echo -e "Add gitignore to the folder path ... ${GREEN}${bold}OK${normal}${NC}"
     
     # Migrating changes to db
     $BASICPYTHONPATH $PROJECTNAME/manage.py makemigrations
     $BASICPYTHONPATH $PROJECTNAME/manage.py migrate
-    echo -e "Changes migrated ... ${GREEN}${bold}OK${normal}${NC}"
+    echo -e "Migrate changes ... ${GREEN}${bold}OK${normal}${NC}"
 
 	echo -e "Project Setup complete ... ${GREEN}${bold}OK${normal}${NC}"
 else
@@ -59,7 +59,7 @@ else
 
 		$BASICPYTHONPATH manage.py startapp $APPNAME
         if [ $? -eq 0 ]; then
-            echo -e "A new app with name $APPNAME created ... ${GREEN}${bold}OK${normal}${NC}"
+            echo -e "Create a new app named $APPNAME ... ${GREEN}${bold}OK${normal}${NC}"
         else
             echo -e "${RED}${bold} Manage.py file not found ${normal}${NC}"
             echo -e "${RED}${bold} Please run this command from your django project directory ${normal}${NC}"
@@ -70,16 +70,19 @@ else
 		touch $APPNAME/forms.py
 		cp $BASIC_DIR/adders/urls.py $APPNAME/
         sed -i -e ':a' -e 'N' -e '$!ba' -e "s/|APPNAME|/$APPNAME/g" $APPNAME/urls.py
-        echo -e "Created other required files ... ${GREEN}${bold}OK${normal}${NC}"
+        echo -e "Create other required files ... ${GREEN}${bold}OK${normal}${NC}"
 
 		# Sets up templates and templates tags
-		mkdir $APPNAME/templates
-		cp $BASIC_DIR/templates/* $APPNAME/templates/ -r
-		mkdir $APPNAME/templatetags/
-		touch $APPNAME/templatetags/__init__.py
-		sed -i "s/|APPNAME|/$APPNAME/g" $APPNAME/templates/base.html
-        sed -i "s/|PAGENAME|/Index/g" $APPNAME/templates/index.html
-		echo -e "Created templates dirs and templates ... ${GREEN}${bold}OK${normal}${NC}"
+        mkdir $APPNAME/templates
+		mkdir $APPNAME/templates/$APPNAME
+		cp $BASIC_DIR/templates/* $APPNAME/templates/$APPNAME -r
+        mkdir $APPNAME/templatetags
+		mkdir $APPNAME/templatetags/$APPNAME
+		touch $APPNAME/templatetags/$APPNAME/__init__.py
+		sed -i "s/|APPNAME|/$APPNAME/g" $APPNAME/templates/$APPNAME/base.html
+		sed -i "s/|APPNAME|/$APPNAME/g" $APPNAME/templates/$APPNAME/index.html
+        sed -i "s/|PAGENAME|/Index/g" $APPNAME/templates/$APPNAME/index.html
+		echo -e "Create templates dirs and templates ... ${GREEN}${bold}OK${normal}${NC}"
 
 		# Sets up management for manual manage.py commands
 		mkdir $APPNAME/management
@@ -98,11 +101,11 @@ else
 
 		# Adding app name to INSTALLED_APPS
         sed -i -e ':a' -e 'N' -e '$!ba' -e "s/INSTALLED_APPS\([^]]*\n*\)*/&\t\'$APPNAME\',\n/g" ./$PROJECTNAME/settings.py
-        echo -e "Added this app into INSTALLED_APPS ... ${GREEN}${bold}OK${normal}${NC}"
+        echo -e "Add this app into INSTALLED_APPS ... ${GREEN}${bold}OK${normal}${NC}"
 
         # Set up urls.py in project folder
-        sed -i "s/urlpatterns = \[/urlpatterns = \[\n\tpath\(\'$APPNAME\', include\(\'$APPNAME\.urls\'\), name=\"$APPNAME\_urls\"\),/g" $PROJECTNAME/urls.py
-        echo -e "Added urls for $APPNAME to project's urls.py ... ${GREEN}${bold}OK${normal}${NC}"
+        sed -i "s/urlpatterns = \[/urlpatterns = \[\n\tpath\(\'$APPNAME\/\', include\(\'$APPNAME\.urls\'\), name=\"$APPNAME\_urls\"\),/g" $PROJECTNAME/urls.py
+        echo -e "Add urls for $APPNAME to project's urls.py ... ${GREEN}${bold}OK${normal}${NC}"
 
         #Set up views.py in app's folder
         echo "def index(request):" >> $APPNAME/views.py
@@ -110,5 +113,46 @@ else
         echo -e "Set up views.py ... ${GREEN}${bold}OK${normal}${NC}"
 
         echo -e "App setup done ... ${GREEN}${bold}OK${normal}${NC}"
+    else
+        if [ $1 == "authenticate" ]
+        then
+            APPNAME="accounts"
+            PROJECTDIR=$(find . -name "settings.py" )
+            PROJECTNAME=$(echo $PROJECTDIR | cut -d'/' -f 2)
+
+            cp $BASIC_DIR/adders/accounts . -r
+            if [ $? -eq 0 ]; then
+                echo -e "Create a new app named $APPNAME ... ${GREEN}${bold}OK${normal}${NC}"
+            else
+                echo -e "${RED}${bold} Manage.py file not found ${normal}${NC}"
+                echo -e "${RED}${bold} Please run this command from your django project directory ${normal}${NC}"
+                exit 5
+            fi
+
+            # Sets up assets and upload folders
+            mkdir static/$APPNAME
+            mkdir media/$APPNAME
+            mkdir static/$APPNAME/css
+            mkdir static/$APPNAME/js
+            mkdir static/$APPNAME/images
+            cp $BASIC_DIR/assets/* static/$APPNAME/ -r
+            echo -e "Set up static and media folders ... ${GREEN}${bold}OK${normal}${NC}"
+
+            # Adding app name to INSTALLED_APPS
+            sed -i -e ':a' -e 'N' -e '$!ba' -e "s/INSTALLED_APPS\([^]]*\n*\)*/&\t\'$APPNAME\',\n/g" ./$PROJECTNAME/settings.py
+            echo -e "Add this app into INSTALLED_APPS ... ${GREEN}${bold}OK${normal}${NC}"
+
+            # Adding URLs in settings.py
+            echo "LOGIN_URL = \"accounts/login/\"" >> $PROJECTNAME/settings.py
+            echo "LOGIN_REDIRECT_URL = \"accounts/\"" >> $PROJECTNAME/settings.py
+            echo "LOGOUT_REDIRECT_URL = \"accounts/\"" >> $PROJECTNAME/settings.py
+            echo -e "Add URLS for easy access in settings.py ... ${GREEN}${bold}OK${normal}${NC} "
+
+            # Set up urls.py in project folder
+            sed -i "s/urlpatterns = \[/urlpatterns = \[\n\tpath\(\'$APPNAME\/\', include\(\'$APPNAME\.urls\'\), name=\"$APPNAME\_urls\"\),/g" $PROJECTNAME/urls.py
+            echo -e "Add urls for $APPNAME to project's urls.py ... ${GREEN}${bold}OK${normal}${NC}"
+
+            echo -e "App setup done ... ${GREEN}${bold}OK${normal}${NC}"
+        fi
 	fi
 fi
